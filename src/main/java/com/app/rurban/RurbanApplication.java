5e2613a49d792d40a535a27f48c00778e4e6b9d2
Aaqib Mir <53465965+aakbmir@users.noreply.github.com>
2024-05-09 22:53:29 +0400

    redirecting to success email verification

--- src/main/java/com/app/rurban/RurbanApplication.java
+++ src/main/java/com/app/rurban/RurbanApplication.java
@@ -4,7 +4,9 @@ import com.app.rurban.services.EmailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,18 +14,19 @@ import org.springframework.web.client.RestTemplate;
 @EnableScheduling
 public class RurbanApplication {
 
-    @Autowired
-    EmailSenderService senderService;
-
     public static void main(String[] args) {
         SpringApplication.run(RurbanApplication.class, args);
     }
 
     @Bean
-    public RestTemplate getRestTemplate(){return  new RestTemplate();}
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
+//    @Autowired
+//    EmailSenderService senderService;
 //    @EventListener(ApplicationReadyEvent.class)
 //    public void sendMail() {
-//        senderService.sendMimeEmail("ezertsoul1617@gmail.com","verification","verify myself test email");
+//        senderService.sendMimeEmail("ezertsoul1617@gmail.com","verification","verifymyselftestemail");
 //    }
 }
